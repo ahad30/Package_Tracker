dc0af1a8a8fe7499d8d660f2d15c0db8f927ebad
ahad30 <mohiminulislamahad@gmail.com>
2025-07-26 00:58:07 +0600

    sddf

--- client/src/components/Layout/HomeLayout/HomeLayout.tsx
+++ client/src/components/Layout/HomeLayout/HomeLayout.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../Common/Header/Header";
-import Footer from "../../Common/Footer/Footer";
 
 const HomeLayout = () => {
     return (
         <>
-            {/* <Header /> */}
+
             <Outlet />
-            {/* <Footer /> */}
         </>
     );
 };
